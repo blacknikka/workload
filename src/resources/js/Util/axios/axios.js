c10508c96bb0b20f72c8485cd1f1308e9f003719
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-06-30 15:58:48 +0900

    front側のログイン処理の対応。 localStrageに保存するように。

--- src/resources/js/Util/axios/axios.js
+++ src/resources/js/Util/axios/axios.js
@@ -1,16 +1,37 @@
 import axios from 'axios';
+import store from '../../store';
 
-class myAxios
-{
-    async get(url) {
-        const result = await axios.get(url);
-        return result;
+class myAxios {
+  async get(url) {
+    const result = await axios.get(url);
+    return result;
+  }
+
+  async post(url, data) {
+    const result = await axios.post(url, data);
+    return result;
+  }
+
+  async auth() {
+    const token = store.getters.loginToken;
+
+    let result;
+    if (token !== '') {
+      result = await axios.post(
+        'api/auth/confirm',
+        {},
+        { headers: { "Authorization": `Bearer ${token}` } }
+      );
     }
 
-    async post(url, data) {
-        const result = await axios.post(url, data);
-        return result;
+    if (result.auth === true) {
+      store.commit('setLoggedIn', {loggedIn: true});
+      return true;
+    } else {
+      store.commit('setLoggedIn', {loggedIn: false});
+      return false;
     }
+  }
 }
 
 export default new myAxios();
--- src/resources/js/views/layout/Login.vue
+++ src/resources/js/views/layout/Login.vue
@@ -1,57 +1,94 @@
 <template>
-    <div class="login-container">
-        <form id="app" @submit="login" method="post">
-            <div class="login-input">
-                e-mail: <input v-model="email" type="text" placeholder="e-mail">
-            </div>
+  <div class="login-container">
+    <form id="app" @submit="login" method="post">
+      <div class="login-input">
+        e-mail:
+        <input v-model="email" type="text" placeholder="e-mail" />
+      </div>
 
-            <div class="login-input">
-                password: <input v-model="password" type="password" placeholder="password">
-            </div>
+      <div class="login-input">
+        password:
+        <input v-model="password" type="password" placeholder="password" />
+      </div>
 
-            <input
-                type="submit"
-                value="Login"
-            >
-        </form>
-        <router-link :to="{name: 'register'}">Register</router-link>
-    </div>
+      <input type="submit" value="Login" />
+    </form>
+    <div class="message-text" v-if="isShownMessage">{{message}}</div>
+    <router-link :to="{name: 'register'}">Register</router-link>
+  </div>
 </template>
 
 <script>
-import axios from '../../Util/axios/axios';
+import axios from "../../Util/axios/axios";
 
 export default {
-    data() {
-        return {
-            email: '',
-            password: '',
-        };
-    },
-    methods: {
-        login(e) {
-            e.preventDefault();
+  data() {
+    return {
+      email: "",
+      password: "",
+      message: ""
+    };
+  },
+  computed: {
+    isShownMessage() {
+      return this.message !== "";
+    }
+  },
+  methods: {
+    async login(e) {
+      e.preventDefault();
 
-            // if (this.email != '' && this.password != '') {
-            //     axios.post('api/logout', {});
-            // }
-        },
-    },
-}
+      // login
+      const result = await axios.post("api/auth/authenticate", {
+        email: this.email,
+        password: this.password
+      });
+
+      if (result.status === 200) {
+        // 成功
+        console.log('login done');
+        this.$store.commit("setLoggedIn", {
+          loggedIn: true,
+          token: result.data.token
+        });
+
+        this.$router.replace({
+          name: 'home',
+        });
+      } else {
+        console.log('login error');
+        this.message = 'login error';
+      }
+    }
+  },
+  mounted() {
+    if (this.$store.getters.loggedIn === true) {
+      const result = axios.auth();
+      if (result === true) {
+        this.$route.replace({ name: "home" });
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .login-container {
-    margin-top: 20vh;
-    width: 100vw;
-    height: 100vh;
-    background-color: honeydew;
+  margin-top: 20vh;
+  width: 100vw;
+  height: 100vh;
+  background-color: honeydew;
 
-    .login-input {
-        text-align: center;
-        width: 80%;
-        height: 10%;
-        background-color: greenyellow;
-    }
+  .login-input {
+    text-align: center;
+    width: 80%;
+    height: 10%;
+    background-color: greenyellow;
+  }
+}
+.message-text {
+  color: red;
+  font-size: 2em;
+  text-align: center;
 }
 </style>

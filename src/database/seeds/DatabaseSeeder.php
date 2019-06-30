d9b99f263ab331efd181949b7263d097af8af882
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-06-30 16:45:56 +0900

    シーディングを調整。

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(WorkloadFKeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(WorkloadTableSeeder::class);
-        $this->call(DepartmentTableSeeder::class);
     }
 }
